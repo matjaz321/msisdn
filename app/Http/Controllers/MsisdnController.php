d9f491d638aa46882a3d752fe84d3fd6062ecf7f
Matjaž Žavski <matjaz.zavski@agiledrop.com>
2018-05-13 18:59:42 +0200

    Changed library to google msisdn library, created basic funcionallity for our controller to get all details from the phone number

--- app/Http/Controllers/MsisdnController.php
+++ app/Http/Controllers/MsisdnController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use libphonenumber\NumberParseException;
+use libphonenumber\PhoneNumberToCarrierMapper;
+use libphonenumber\PhoneNumberUtil;
 
 class MsisdnController extends Controller {
 
@@ -12,5 +15,34 @@ class MsisdnController extends Controller {
       'number' => 'required|regex:/^\+?\d{6,7}[2-9]\d{3}$/',
     ]);
 
+    // Retrieve the validated input data...
+    $number = $request->number;
+
+    // Define classes that we need to use.
+    $phoneUtil = PhoneNumberUtil::getInstance();
+    $phoneNumberCarrier = PhoneNumberToCarrierMapper::getInstance();
+
+    try {
+      // Define PhoneNumber instance.
+      $phoneNumber = $phoneUtil->parse($number);
+      // Check if phone number is valid.
+      if ($phoneUtil->isValidNumber($phoneNumber)) {
+        // Get phone number parameters.
+        $countryCode = $phoneUtil->getRegionCodeForNumber($phoneNumber);
+        $carrier = $phoneNumberCarrier->getNameForNumber($phoneNumber, $countryCode);
+        $countryDialingCode = $phoneNumber->getCountryCode();
+        $subscriberNumber = $phoneNumber->getNationalNumber();
+
+        // We will return this data to the view.
+        $data = [
+          'MNO' => $carrier,
+          'country_dialing_code' => $countryDialingCode,
+          'subscriber_number' => $subscriberNumber,
+          'country_code' => $countryCode,
+        ];
+      }
+    } catch (NumberParseException $e) {
+      // Define errors.
+    }
   }
 }
